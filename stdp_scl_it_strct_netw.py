4b5053355e1850ae1ba2f6ac2a9a6cc8561b0c06
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-12-18 18:49:10 +0100

    save number of active synapses in SynAct_stat

--- stdp_scl_it_strct_netw.py
+++ stdp_scl_it_strct_netw.py
@@ -209,10 +209,8 @@ def run_net(tr):
     GExc_vts.record_single_timestep()
     SynEE_a.record_single_timestep()
 
-    tr.f_add_result('SynAct_stat', {'no': active_synapses})
-
-    
     tr.v_standard_result = Brian2MonitorResult
+    tr.f_add_result('SynAct_stat', {'no': active_synapses})
     tr.f_add_result('GExc_stat', GExc_stat)
     tr.f_add_result('SynEE_stat', SynEE_stat)
     print("Saving exc spikes...   ", GExc_spks.get_states()['N'])
