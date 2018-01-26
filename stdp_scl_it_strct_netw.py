d6676358a48245bcd15bf76d703c898f3980cc42
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-01-26 12:28:52 +0100

    if strct: set synEE.a = -1 to track

--- stdp_scl_it_strct_netw.py
+++ stdp_scl_it_strct_netw.py
@@ -162,7 +162,11 @@ def run_net(tr):
     tr.f_add_result('sII_src', sII_src)
     tr.f_add_result('sII_tar', sII_tar)
 
-    SynEE.a = tr.a_ee
+    if tr.strct_active:
+        SynEE.a = -1 # allows to track synapses that were never inserted
+    else:
+        SynEE.a = tr.a_ee
+        
     SynEE.insert_P = tr.insert_P
 
     # synaptic scaling
