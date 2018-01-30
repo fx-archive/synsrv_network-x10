ee9079dddd068f6bbe316dbb4e90f52b2b0e6181
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-01-30 17:32:04 +0100

    add run_id to tmp_turnover

--- stdp_scl_it_strct_netw.py
+++ stdp_scl_it_strct_netw.py
@@ -111,23 +111,23 @@ def add_params(tr):
 @implementation('cpp', code=r'''
 #include <fstream>
 
-double record_turnover(double t, int was_active_before, int should_become_active, int should_stay_active, int syn_active, int i, int j) {
+double record_turnover(double t, int was_active_before, int should_become_active, int should_stay_active, int syn_active, int i, int j, int run_id) {
     if (int(was_active_before==0)*should_become_active==1){
         std::ofstream outfile;
         outfile.open("../tmp_turnover", std::ios_base::app);
-        outfile << 1 << "," << t << "," << i << "," << j << "\n";}
+        outfile << 1 << "," << t << "," << i << "," << j << run_id << "\n";}
     else if (was_active_before*int(should_stay_active==0)){
         std::ofstream outfile;
         outfile.open("../tmp_turnover", std::ios_base::app);
-        outfile << 0 << "," << t << "," << i << "," << j << "\n";}
+        outfile << 0 << "," << t << "," << i << "," << j << run_id << "\n";}
 
     return 0.0; // we need to return a dummy value
 } '''
                 )
 @check_units(t=second, was_active_before=1, should_become_active=1,
-             should_stay_active=1, syn_active=1, i=1, j=1, result=1)
+             should_stay_active=1, syn_active=1, i=1, j=1, run_id=1, result=1)
 def record_turnover(t, was_active_before, should_become_active,
-                    should_stay_active, syn_active, i, j):
+                    should_stay_active, syn_active, i, j, run_id):
     return 0.0
 
     
