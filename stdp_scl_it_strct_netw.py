c0b95ade01cf2dcbac4d54629e24ade5319c95a5
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-01-30 18:10:11 +0100

    need variable name?

--- stdp_scl_it_strct_netw.py
+++ stdp_scl_it_strct_netw.py
@@ -115,7 +115,8 @@ double record_turnover(double t, int was_active_before, int should_become_active
 
     if (int(was_active_before==0)*should_become_active==1){
         std::ofstream outfile;
-        std::string fname = "../tmp_turnover_" + std::to_string (run_id);
+        std::string dirpath = "../tmp_turnover_";
+        std::string fname = dirpath  + std::to_string (run_id);
 
         outfile.open(fname, std::ios_base::app);
 
@@ -124,7 +125,8 @@ double record_turnover(double t, int was_active_before, int should_become_active
 
     else if (was_active_before*int(should_stay_active==0)){
         std::ofstream outfile;
-        std::string fname = "../tmp_turnover_" + std::to_string (run_id);
+        std::string dirpath = "../tmp_turnover_";
+        std::string fname = dirpath  + std::to_string (run_id);
 
         outfile.open(fname, std::ios_base::app);
 
