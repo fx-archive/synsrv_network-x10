cdca2b87b3e29ed0136b1cf4c3b44462d6a75b33
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-01-31 09:35:16 +0100

    save data in build directory

--- stdp_scl_it_strct_netw.py
+++ stdp_scl_it_strct_netw.py
@@ -115,12 +115,12 @@ def add_params(tr):
 
       if (int(was_active_before==0)*should_become_active==1){
           std::ofstream outfile;          
-          outfile.open("tmp_data/turnover", std::ios_base::app);
+          outfile.open("./tmp_data/turnover", std::ios_base::app);
           outfile << 1 << "," << t << "," << i << "," << j << "\n";
       }
       else if (was_active_before*int(should_stay_active==0)){
            std::ofstream outfile;     
-           outfile.open("tmp_data/turnover", std::ios_base::app);
+           outfile.open("./tmp_data/turnover", std::ios_base::app);
            outfile << 0 << "," << t << "," << i << "," << j << "\n";
       }
 
