e4ffdc1234591d08f6a5e90951f63314423fd6d8
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-01-31 09:03:01 +0100

    nothing

--- stdp_scl_it_strct_netw.py
+++ stdp_scl_it_strct_netw.py
@@ -109,38 +109,7 @@ def add_params(tr):
 #     return 0.0
 
 @implementation('cpp', code=r'''
-//#include <fstream>
-//#include <string>
-
 double record_turnover(double t, int was_active_before, int should_become_active, int should_stay_active, int syn_active, int i, int j, int run_id) {
-
-//std::string name = "John";
-//int age = 21;
-//std::string result;
-//result = name + std::to_string(age);
-
-  //  if (int(was_active_before==0)*should_become_active==1){
-  //      std::ofstream outfile;
-  //      int i = 4;
-  //      std::string text "Player ";      
-  //      text += std::to_string(i);
-
-        //outfile.open("../tmp_this", std::ios_base::app);
-
-        //outfile << 1 << "," << t << "," << i << "," << j << run_id << "\n";
- //   }
-
- //   else if (was_active_before*int(should_stay_active==0)){
- //       std::ofstream outfile;
- //      int i = 4;
- //       std::string text "Player ";      
- //       text += std::to_string(i);
-
-        //outfile.open("../tmp_this", std::ios_base::app);
-
-        //outfile << 0 << "," << t << "," << i << "," << j << run_id << "\n";
- //   }
-
     return 0.0; // we need to return a dummy value
 } '''
                 )
