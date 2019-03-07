e737aa53b1d0f137a32283679e4aa444c8f215cb
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-03-07 15:03:10 +0100

    correlation analysis

--- xstrct_netw.py
+++ xstrct_netw.py
@@ -429,11 +429,11 @@ def run_net(tr):
         net = Network(GExc, GInh, PInp, sPN, sPNInh, SynEE, SynEI, SynIE, SynII,
                       GExc_stat, GInh_stat, SynEE_stat, SynEE_a,
                       GExc_spks, GInh_spks, PInp_spks, GExc_rate, GInh_rate,
-                      PInp_rate, PInp_inh, foo)
+                      PInp_rate, PInp_inh)
     else:
         net = Network(GExc, GInh, SynEE, SynEI, SynIE, SynII,
                       GExc_stat, GInh_stat, SynEE_stat, SynEE_a,
-                      GExc_spks, GInh_spks, GExc_rate, GInh_rate, foo)
+                      GExc_spks, GInh_spks, GExc_rate, GInh_rate)
 
 
     spks_recorders = [GExc_spks, GInh_spks]
