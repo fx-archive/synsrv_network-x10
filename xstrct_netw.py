112a9b44b62f9c8c356e4b8d93d787d02aa3964d
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-04-05 14:43:23 +0200

    debug new simualtion periods - right clocks

--- xstrct_netw.py
+++ xstrct_netw.py
@@ -190,16 +190,16 @@ def run_net(tr):
 
 
     # recording of stdp in T4
-    SynEE.stdp_rec_start = tr.T1+tr.T2+tr.T3+tr.T4
-    SynEE.stdp_rec_max = tr.T1+tr.T2+tr.T3+tr.T4 + tr.stdp_rec_T
+    SynEE.stdp_rec_start = tr.T1+tr.T2+tr.T3
+    SynEE.stdp_rec_max = tr.T1+tr.T2+tr.T3 + tr.stdp_rec_T
 
        
     # synaptic scaling
     if tr.netw.config.scl_active:
 
         if tr.syn_scl_rec:
-            SynEE.scl_rec_start = tr.T1+tr.T2+tr.T3+tr.T4
-            SynEE.scl_rec_max = tr.T1+tr.T2+tr.T3+tr.T4 + tr.scl_rec_T
+            SynEE.scl_rec_start = tr.T1+tr.T2+tr.T3
+            SynEE.scl_rec_max = tr.T1+tr.T2+tr.T3 + tr.scl_rec_T
         else:
             SynEE.scl_rec_start = T+10*second
             SynEE.scl_rec_max = T*second
