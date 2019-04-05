6057e5539563d2de741b69e9193ba223e98a7853
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-04-05 10:13:16 +0200

    if syn_scl_rec, record after changes through scaling after tr.T3 (fix) #6

--- xstrct_netw.py
+++ xstrct_netw.py
@@ -193,9 +193,9 @@ def run_net(tr):
     if tr.netw.config.scl_active:
 
         if tr.syn_scl_rec:
-            synEE.rec_start = tr.T1+tr.T2+tr.T3
+            SynEE.rec_start = tr.T1+tr.T2+tr.T3
         else:
-            synEE.rec_start = tr.T1+tr.T2+tr.T3+tr.T4+(10*second)
+            SynEE.rec_start = tr.T1+tr.T2+tr.T3+tr.T4+(10*second)
         
         SynEE.summed_updaters['Asum_post']._clock = Clock(
             dt=tr.dt_synEE_scaling)
