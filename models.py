e423ce059f7bccdbc7a8e1cc4a50f460ac2852d7
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-04-24 20:13:36 +0200

    implement as run_regularly [#9]

--- models.py
+++ models.py
@@ -36,7 +36,7 @@ poisson_mod = 'PInp_a : 1'
 # dApost /dt = -Apost/taupost : 1 (event-driven)
 
 
-synEE_noise  = 'a += syn_active*syn_sigma*xi : 1'
+synEE_noise  = 'a += syn_active*syn_sigma*xi'
 
 synEE_mod = '''
             a : 1
