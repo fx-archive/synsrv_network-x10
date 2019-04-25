ea182436a2473891fa59e61247f8fc1c7c431704
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-04-25 10:01:41 +0200

    synEE updates every dt=2*second [#9]

--- xstrct_netw.py
+++ xstrct_netw.py
@@ -153,7 +153,7 @@ def run_net(tr):
     # E<-E advanced synapse model, rest simple
     SynEE = Synapses(target=GExc, source=GExc, model=synEE_mod,
                      on_pre=synEE_pre_mod, on_post=synEE_post_mod,
-                     namespace=namespace, dt=25*ms)
+                     namespace=namespace, dt=2*second)
     SynIE = Synapses(target=GInh, source=GExc, on_pre='ge_post += a_ie',
                      namespace=namespace)
     SynEI = Synapses(target=GExc, source=GInh, on_pre='gi_post += a_ei',
